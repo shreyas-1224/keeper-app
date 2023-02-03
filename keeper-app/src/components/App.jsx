37073c19362ad11f46678bd5bdc401227c372b20
shreyas <pshreyas1224@gmail.com>
2023-02-03 16:09:50 +0530

    added state in input area

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
+import Footer from "./Footer.jsx";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
--- keeper-app/src/components/CreateArea.jsx
+++ keeper-app/src/components/CreateArea.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 
 function CreateArea() {
+
+  const [note , setNote] = useState({
+    title: "",
+    content:""
+  });
+
+  function handleChange(event){
+    const {name , value} = event.target;
+    if(name === "title"){
+        setNote((prevValue)=>{
+            return({
+                title:value,
+                content:prevValue.content
+            });
+        });
+    }else if(name === "content"){
+        setNote((prevValue)=>{
+            return({
+                content:value,
+                title:prevValue.title
+            });
+        });
+    }
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input onChange={handleChange} name="title" placeholder="Title" />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
